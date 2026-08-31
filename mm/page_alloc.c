1019a0aa725806d5bc7b8242fa31d6a62b13b765
//. <174224977+skye-tachyon@users.noreply.github.com>
2026-08-31 13:11:52 -0300

    fixup! 'mm: cleanup memory boosting remains'
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4406,7 +4406,8 @@ retry:
 	 */
 	if (!page && !drained) {
 		unreserve_highatomic_pageblock(ac, false);
-		drain_all_pages(NULL);
+		if (!task_is_critical())
+			drain_all_pages(NULL);
 		drained = true;
 		goto retry;
 	}
